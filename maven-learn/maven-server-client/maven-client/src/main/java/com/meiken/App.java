bd4aa9e1d534209113fdd6dd5ed0c31778ecd950
Meiken <aluo_fei@163.com>
2021-11-05 00:24:36 +0800

    maven

---
+++ maven-learn/maven-server-client/maven-client/src/main/java/com/meiken/App.java
@@ -0,0 +1,16 @@
+package com.meiken;
+
+import lombok.Data;
+
+/**
+ * Hello world!
+ *
+ */
+@Data
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
