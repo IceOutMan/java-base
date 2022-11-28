8c52cf5d9dc5c279ce4366eedc84ae0a9e41814b
IceOutMan <aluo_fei@163.com>
2022-11-28 14:05:51 +0800

    add
    add

---
+++ jvmlearn/src/main/java/com/meiken/load/TestJDKClassLoader.java
@@ -0,0 +1,10 @@
+package com.meiken.load;
+
+public class TestJDKClassLoader {
+    public static void main(String[] args) {
+        System.out.println(String.class.getClassLoader());
+        System.out.println(com.sun.crypto.provider.DESKeyFactory.class.getClassLoader().getClass().getName());
+        System.out.println(TestJDKClassLoader.class.getClassLoader().getClass().getName());
+    }
+
+}
