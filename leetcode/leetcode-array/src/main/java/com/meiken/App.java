ad81b5a6a085ec38cbdfcde49edbcbaf0dd48a29
IceOutMan <aluo_fei@163.com>
2023-04-12 18:36:02 +0800

    add: other info

---
+++ leetcode/leetcode-array/src/main/java/com/meiken/App.java
@@ -0,0 +1,17 @@
+package com.meiken;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HelljHo World!" );
+    }
+
+}
+
+
+
