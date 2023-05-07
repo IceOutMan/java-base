3815cbddd56740a4e84206fc933a5344e41cb70c
IceOutMan <ainewbeginglf@gmail.com>
2023-05-07 10:42:42 +0800

    test
    test

--- leetcode/leetcode-link/src/main/java/com/meiken/App.java
+++ leetcode/leetcode-link/src/main/java/com/meiken/App.java
@@ -8,6 +8,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        permute(new int[]{1,2,3,4,5}, 0);
+    }
+
+    public static void permute(int[] nums, int index){
+        if(index == nums.length){
+            // 已经无可替换
+            for(int num : nums){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+
+        for(int i=index; i<nums.length; i++){
+            // 占据位置 index
+            int temp = nums[index];
+            nums[index] = nums[i];
+            nums[i] =temp;
+
+            permute(nums, index+1);
+
+            nums[i] = nums[index];
+            nums[index] = temp;
+        }
+
     }
 }
