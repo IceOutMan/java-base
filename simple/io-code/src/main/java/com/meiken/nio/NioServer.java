b3de475d7bc4ce0628a2b071d11527cdd7c6a77f
IceOutMan <aluo_fei@163.com>
2023-10-12 16:50:37 +0800

    modify: nio

--- simple/io-code/src/main/java/com/meiken/nio/NioServer.java
+++ simple/io-code/src/main/java/com/meiken/nio/NioServer.java
@@ -3,52 +3,105 @@ package com.meiken.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class NioServer {
 
-    static HashSet<SocketChannel> channelSet = new HashSet<>();
+    private static ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
 
     public static void main(String[] args) throws IOException {
-        // 创建 NIO ServerSocketChannel ， 与BIO的 serverSocket 类似
-        ServerSocketChannel serverSocket = ServerSocketChannel.open();
-        serverSocket.socket().bind(new InetSocketAddress(9000));
-        // 设置 ServerSocketChannel 为非阻塞方式
-        serverSocket.configureBlocking(false);
-        System.out.println("server start");
-        while(true){
-            // 非阻塞模式 accept 方法不会阻塞
-            // NIO 的非阻塞由操作系统内部实现，底层调用了 Linux 内核的 accept 函数
-            SocketChannel socketChannel = serverSocket.accept();
-            if(socketChannel != null){
-                System.out.println("client connection success");
-                // 设置 SocketChannel 为非阻塞
-                socketChannel.configureBlocking(false);
+        try (Selector selector = Selector.open()) {
 
-                channelSet.add(socketChannel);
-            }
+            // ServerSocketChannel  ->  ServerSocket
+            // SocketChannel -> Socket
+            ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.socket().bind(new InetSocketAddress(9000));
+            // 设置 ServerSocketChannel 为非阻塞方式
+            serverSocket.configureBlocking(false);
 
-            // 遍历连接进行数据读取
-            Iterator<SocketChannel> iterator = channelSet.iterator();
-            while (iterator.hasNext()){
-                SocketChannel sc = iterator.next();
-                ByteBuffer byteBuffer = ByteBuffer.allocate(128);
-                // 非阻塞模式 read 方法不会阻塞
-                int len = sc.read(byteBuffer);
-                if(len > 0){
-                    // 如果有数据 打印数据
-                    System.out.println("server receive : " + new String(byteBuffer.array()));
-                }else if(len == -1){
-                    // 如果客户端断开，socket 从集合中移除
-                    channelSet.remove(sc);
-                    System.out.println("client close connection");
+            // serverSocket 注册为 ACCEPT 事件，改 socket只关心 accept 事件
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("server start");
+            while (true) {
+                // 阻塞在 select 这里, 等待就绪的 channel
+                selector.select();
+
+                // 发生了事件的 key
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> selectionKeyIter = selectionKeys.iterator();
+                while(selectionKeyIter.hasNext()){
+                    SelectionKey key = selectionKeyIter.next();
+
+                    // 如果是 accept 事件
+                    if(key.isAcceptable()){
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        // 拿到发起连接的客户端的 SocketChannel
+                        SocketChannel socketChannel = server.accept();
+
+                        // 注册 SocketChannel -> Selector｜READ
+                        registerChannel(selector, socketChannel, SelectionKey.OP_READ);
+                        // 向客户端发送一条打招呼的消息
+                        sayHello(socketChannel);
+                    }
+
+                    // 如果是 read 事件
+                    if(key.isReadable()){
+                        readDataFromSocket(key);
+                    }
+
+                    // 从事件集合中移除本次处理的 key，防止下次 select 重复处理
+                    selectionKeyIter.remove();
                 }
-
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void registerChannel(Selector selector, SocketChannel socketChannel, int ops) throws Exception{
+        if(socketChannel == null){
+            return;
         }
 
+        // 设置 channel 非阻塞
+        socketChannel.configureBlocking(false);
+
+        // 注册到 selector
+        socketChannel.register(selector, ops);
+    }
+
+    public static void readDataFromSocket(SelectionKey key) throws Exception{
+        SocketChannel socketChannel = (SocketChannel) key.channel();
+        int count;
+        buffer.clear();
+
+        while((count = socketChannel.read(buffer)) > 0){
+            buffer.flip();
+
+            // 你说啥我也说啥
+            while(buffer.hasRemaining()){
+                socketChannel.write(buffer);
+            }
+
+            buffer.clear();
+        }
+
+        // 通道已关闭
+        if(count < 0){
+            socketChannel.close();
+        }
+    }
+
+    private static void sayHello(SocketChannel channel) throws Exception{
+        buffer.clear();
+        buffer.put("Hi there !\r\n".getBytes());
+        buffer.flip();
+        channel.write(buffer);
     }
 }
